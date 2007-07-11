1d3427fd1b10eefd4a7d20aabacda57bbbdbe27f
davidd <davidd@18b564fe-9b33-0410-88e2-c23dde86dd5c>
2007-07-11 05:12:21 +0000

    cleaned some comments

--- src/test/java/org/mule/providers/jcr/JcrContentPayloadTypeTestCase.java
+++ src/test/java/org/mule/providers/jcr/JcrContentPayloadTypeTestCase.java
@@ -1,15 +1,21 @@
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSource, Inc.  All rights reserved.  http://www.mulesource.com
+ *
+ * The software in this package is published under the terms of the MuleSource MPL
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
 package org.mule.providers.jcr;
 
 import junit.framework.TestCase;
 
 /**
- * TODO comment
- *
  * @author David Dossot (david@dossot.net)
  */
 public class JcrContentPayloadTypeTestCase extends TestCase {
-
-    // FIXME test equals / hashcode
 
     public void testFromStringLowerCaseEquals() {
         assertEquals(JcrContentPayloadType.NONE,
