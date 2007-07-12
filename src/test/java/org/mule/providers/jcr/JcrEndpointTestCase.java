b5c75194e874514b438344079356ed9a9254de11
davidd <david@dossot.net>
2007-07-12 04:46:47 +0000

    commented

--- src/test/java/org/mule/providers/jcr/JcrEndpointTestCase.java
+++ src/test/java/org/mule/providers/jcr/JcrEndpointTestCase.java
@@ -14,21 +14,22 @@ import org.mule.impl.endpoint.MuleEndpointURI;
 import org.mule.tck.AbstractMuleTestCase;
 import org.mule.umo.endpoint.UMOEndpointURI;
 
-public class JcrEndpointTestCase extends AbstractMuleTestCase
-{
+/**
+ * @author David Dossot (david@dossot.net)
+ */
+public class JcrEndpointTestCase extends AbstractMuleTestCase {
 
-    /* For general guidelines on writing transports see
-       http://mule.mulesource.org/display/MULE/Writing+Transports */
+	public void testValidEndpointURI() throws Exception {
+		UMOEndpointURI uri = new MuleEndpointURI(
+				"jcr://path/to/observedNode?eventTypes=5");
 
-    public void testValidEndpointURI() throws Exception
-    {
-        UMOEndpointURI uri = new MuleEndpointURI("jcr://path/to/observedNode?eventTypes=5");
-
-        assertEquals("jcr", uri.getScheme());
-        assertEquals("jcr://path/to/observedNode?eventTypes=5", uri.getAddress());
-        assertEquals("path/to/observedNode", uri.getHost() + uri.getPath());
-        assertEquals(1, uri.getParams().size());
-        assertEquals("5", uri.getParams().getProperty("eventTypes"));
-    }
+		assertEquals("jcr", uri.getScheme());
+		assertEquals("jcr://path/to/observedNode?eventTypes=5", uri
+				.getAddress());
+		
+		assertEquals("path/to/observedNode", uri.getHost() + uri.getPath());
+		assertEquals(1, uri.getParams().size());
+		assertEquals("5", uri.getParams().getProperty("eventTypes"));
+	}
 
 }
